f2555ecb5efac1e58048e02df310ceb3cf13d185
keshav01m <keshav01m@gmail.com>
2024-06-23 03:36:37 +0530

    icons added, somu ui changes

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Sidebar from "./components/Sidebar/Sidebar.jsx";
+
+const app = () => {
+  return (
+    <>
+      <Sidebar/>
+    </>
+  );
+}
+
+export default app;
